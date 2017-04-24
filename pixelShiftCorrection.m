a948164c8b5d12b713e188770063da098c261ec0
gpoornima25 <poornima.gadamsetty@gmail.com>
2017-04-24 19:47:16 -0400

    Update pixelShiftCorrection.m

--- pixelShiftCorrection.m
+++ pixelShiftCorrection.m
@@ -1,9 +1,7 @@
 %% 
 figure
 subplot(211)
-% m=(double(GL1(:,:,1)));
-% m1=mean(double(m),3);
-m1=(mean(double(GL1),3));
+m1=(mean(double(GL1),3)); % GL1 is the image
 imagesc(m1)
 
 %Pixel shift correction (shift by one pixel for alternate odd rows)
@@ -18,39 +16,3 @@ for j=1:2:nRow
     end
 end
 imagesc(m2)
-
- %%
-%  %% 
-% 
-% subplot(311)
-% m=(double(GL1(100:150,100:150,:)));
-% m1=mean(double(m),3);
-% imagesc(m1)
-% 
-% %shift by one pixel for alternate even rows
-% subplot(312)
-% m2=m1
-% 
-% n=size(m2);
-% nCol=n(2)
-% nRow=n(1)
-% for j=2:2:nRow
-%     for i=1: nCol-1
-%              m2(j,i)=m2(j,i+1);
-%     end
-% end
-% imagesc(m2)
-% 
-% %shift by 2 pixels for alternate even rows
-% subplot(313)
-% m2=m1
-% 
-% n=size(m2);
-% nCol=n(2)
-% nRow=n(1)
-% for j=2:2:nRow
-%     for i=1: nCol-2
-%              m2(j,i)=m2(j,i+2);
-%     end
-% end
-% imagesc(m2)
